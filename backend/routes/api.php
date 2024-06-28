ce44872134d5b0cdc9d1ed98c158673b88d08a7a
StashaSesevic <160345537+Stasa99@users.noreply.github.com>
2024-06-28 14:28:40 +0200

    Revert "dodate funkcije u TipDogadjajaController"
    This reverts commit 1ec64cb51d97657257242eead80bff845bf7ea8d.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TipDogadjajaController;
+use App\Models\TipDogadjaja;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,8 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Tipovi Dogadjaja Routes
 Route::get('/tipoviDogadjaja', [TipDogadjajaController::class, 'index']);
-Route::post('/tipoviDogadjaja', [TipDogadjajaController::class, 'store']);
-Route::put('/tipoviDogadjaja/{id}', [TipDogadjajaController::class, 'update']);
-Route::delete('/tipoviDogadjaja/{id}', [TipDogadjajaController::class, 'destroy']);
